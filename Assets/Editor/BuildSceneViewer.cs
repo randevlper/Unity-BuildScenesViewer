260ec1d9f7d2185e078b3086030ea72b4b601c36
Randall Hines <randallhines0@gmail.com>
2019-04-11 10:24:42 -0700

    -Init repo

---
+++ Assets/Editor/BuildSceneViewer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class BuildSceneViewer : EditorWindow {
+
+	[MenuItem ("Window/BuildSceneViewer")]
+	private static void ShowWindow () {
+		var window = GetWindow<BuildSceneViewer> ();
+		window.titleContent = new GUIContent ("BuildSceneViewer");
+		window.Show ();
+	}
+
+	string[] levels;
+	string[] levelPaths;
+
+	private void OnGUI () {
+		if (GUILayout.Button ("Generate")) {
+			levelPaths = BuildSceneViewerUtil.ReadPaths ();
+			levels = BuildSceneViewerUtil.ReadNames();
+		}
+		GUILayout.Label ("Scenes", EditorStyles.largeLabel);
+		if (levels != null) {
+			for (int i = 0; i < levels.Length; i++) {
+				if (GUILayout.Button (levels[i])) {
+					UnityEditor.SceneManagement.EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo();
+					UnityEditor.SceneManagement.EditorSceneManager.OpenScene (levelPaths[i]);
+				}
+			}
+		}
+	}
+}
---
+++ Assets/Editor/BuildSceneViewerUtil.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace UnityEditor {
+	public class BuildSceneViewerUtil {
+		public static string[] ReadNames () {
+			List<string> temp = new List<string> ();
+			foreach (UnityEditor.EditorBuildSettingsScene S in UnityEditor.EditorBuildSettings.scenes) {
+				if (S.enabled) {
+					string name = S.path.Substring (S.path.LastIndexOf ('/') + 1);
+					name = name.Substring (0, name.Length - 6);
+					temp.Add (name);
+				}
+			}
+			return temp.ToArray ();
+		}
+
+		public static string[] ReadPaths () {
+			List<string> temp = new List<string> ();
+			foreach (UnityEditor.EditorBuildSettingsScene S in UnityEditor.EditorBuildSettings.scenes) {
+				if (S.enabled) {
+					//string name = S.path.Substring (S.path.LastIndexOf ('/') + 1);
+					//name = name.Substring (0, name.Length - 6);
+					temp.Add (S.path);
+				}
+			}
+			return temp.ToArray ();
+
+		}
+	}
+}
